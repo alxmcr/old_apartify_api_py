af54a69ba2e820aa4d96d2fbe667bd987302798c
Alejandro M. Coca <alxmcr@outlook.com>
2021-08-12 18:11:36 -0400

    Improvements with our model - relationships

--- api/models.py
+++ api/models.py
@@ -31,10 +31,10 @@ Relationships
  - (N Apart <- has -- 1 Investment)
  => (N:N - "ApartHasInvestment")
  
-* Apartment - Pleasantness 
- - (1 Apart -- has -> N Pleasantness)
- - (N Apart <- has -- 1 Pleasantness)
- => (N:N - "ApartHasPleasantness")
+* Apartment - OutdoorSpace 
+ - (1 Apart -- has -> N OutdoorSpace)
+ - (N Apart <- has -- 1 OutdoorSpace)
+ => (N:N - "ApartHasOutdoorSpace")
 
 """
 
@@ -78,16 +78,6 @@ class Neighborhood(models.Model):
     def __str__(self):
         return f"{self.ne_neighborhood}. {self.ne_name}"
 
-class Street(models.Model):
-    st_street = models.BigAutoField(primary_key=True)
-    st_name = models.CharField("Name", max_length=50, null=False)
-    st_abbr = models.CharField("Abbreviation", max_length=5, null=False)
-    st_url = models.CharField("URL Photo", max_length=80)
-    st_alt = models.CharField("Photo Alternative Text", max_length=80)
-
-    def __str__(self):
-        return f"{self.st_street}. {self.st_name}"
-
 class Feature(models.Model):
     fe_feature = models.BigAutoField(primary_key=True)
     fe_type = models.CharField("Type", max_length=50, null=False)
@@ -100,6 +90,45 @@ class Feature(models.Model):
     def __str__(self):
         return f"{self.fe_feature}. {self.fe_type} - {self.fe_name}"
 
+class Apartment(models.Model):
+    ap_apartment = models.BigAutoField(primary_key=True)
+    ap_descripcion = models.CharField("Description", max_length=50, null=False)
+    ap_floor_number = models.IntegerField("Floor Number", null=False, default=0)
+    ap_cost_offer = models.CharField("Offer Cost", max_length=50)
+    ap_cost_list = models.CharField("List cost", max_length=50, null=False)
+    ap_is_remodeling = models.BooleanField("Is it remodeling?", null=False)
+    ap_latitude = models.DecimalField("Latitude", decimal_places=4, max_digits=4, null=False)
+    ap_longitude = models.DecimalField("Longitude", decimal_places=4, max_digits=4, null=False)
+    ap_street_name = models.CharField("Street Name", max_length=80, null=False, default='')
+    ap_ext_number = models.CharField("Exterior Number", max_length=10, null=False, default=0)
+    ap_int_number = models.CharField("Interior Number", max_length=10, null=False, default=0)
+    ne_neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+    ci_city = models.ForeignKey(City, on_delete=models.CASCADE)
+    co_country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    st_state = models.ForeignKey(State, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.ap_apartment}. {self.ap_descripcion}"
+
+class Photo(models.Model):
+    ph_photo = models.BigAutoField(primary_key=True)
+    ph_url = models.CharField("URL Photo", max_length=80, null=False)
+    ph_alt = models.CharField("Photo Alternative Text", max_length=80, null=False)
+    ap_apartment = models.ForeignKey(Apartment, on_delete=models.CASCADE, null=False)
+
+    def __str__(self):
+        return f"{self.ph_photo}. {self.ph_url}"
+
+class Flat(models.Model):
+    fl_flat = models.BigAutoField(primary_key=True)
+    fl_url = models.CharField("URL Flat", max_length=80, null=False)
+    fl_alt = models.CharField("Flat Alternative Text", max_length=80, null=False)
+    ap_apartment = models.ForeignKey(Apartment, on_delete=models.CASCADE, null=False)
+
+    def __str__(self):
+        return f"{self.fl_flat}. {self.fl_url}"
+
+# N:N + intermediate table
 class Investment(models.Model):
     in_investment = models.BigAutoField(primary_key=True)
     in_type = models.CharField("Type", max_length=30, null=False)
@@ -112,39 +141,21 @@ class Investment(models.Model):
     def __str__(self):
         return f"{self.in_investment}. {self.in_type} - {self.in_name}"
 
-class Pleasantness(models.Model):
-    pl_pleasantness = models.BigAutoField(primary_key=True)
-    pl_type = models.CharField("Type", max_length=30, null=False)
-    pl_name = models.CharField("Name", max_length=30, null=False)
-    pl_icon_url = models.CharField("URL Icon", max_length=80, null=False)
-    pl_icon_color = models.CharField("Color Icon", max_length=80, null=False)
-    pl_is_visible = models.BooleanField("Is it visible?", null=False, default=True)
-    pl_is_card = models.BooleanField("Is it in a card?", null=False, default=True)
+class Invest(models.Model):
+    ap_apartment = models.ForeignKey(Apartment, on_delete=models.CASCADE)
+    in_investment = models.ForeignKey(Investment, on_delete=models.CASCADE)
+    inv_value = models.CharField("Value", max_length=10, null=False)
 
-class Apartment(models.Model):
-    ap_apartment = models.BigAutoField(primary_key=True)
-    ap_descripcion = models.CharField("Description", max_length=50, null=False)
-    ap_cost_offer = models.CharField("Offer Cost", max_length=50)
-    ap_cost_list = models.CharField("List cost", max_length=50, null=False)
-    ap_is_remodeling = models.BooleanField("Is it remodeling?", null=False)
-    ap_latitude = models.DecimalField("Latitude", decimal_places=4, max_digits=4, null=False)
-    ap_longitude = models.DecimalField("Longitude", decimal_places=4, max_digits=4, null=False)
+class OutdoorSpace(models.Model):
+    ou_outdoor_space = models.BigAutoField(primary_key=True)
+    ou_type = models.CharField("Type", max_length=30, null=False)
+    ou_name = models.CharField("Name", max_length=30, null=False)
+    ou_icon_url = models.CharField("URL Icon", max_length=80, null=False)
+    ou_icon_color = models.CharField("Color Icon", max_length=80, null=False)
+    ou_is_visible = models.BooleanField("Is it visible?", null=False, default=True)
+    ou_is_card = models.BooleanField("Is it in a card?", null=False, default=True)
 
-    def __str__(self):
-        return f"{self.ap_apartment}. {self.ap_descripcion}"
-
-class Photo(models.Model):
-    ph_photo = models.BigAutoField(primary_key=True)
-    ph_url = models.CharField("URL Photo", max_length=80, null=False)
-    ph_alt = models.CharField("Photo Alternative Text", max_length=80, null=False)
-
-    def __str__(self):
-        return f"{self.ph_photo}. {self.ph_url}"
-
-class Flat(models.Model):
-    fl_flat = models.BigAutoField(primary_key=True)
-    fl_url = models.CharField("URL Flat", max_length=80, null=False)
-    fl_alt = models.CharField("Flat Alternative Text", max_length=80, null=False)
-
-    def __str__(self):
-        return f"{self.fl_flat}. {self.fl_url}"
+class Outdoor(models.Model):
+    ap_apartment = models.ForeignKey(Apartment, on_delete=models.CASCADE)
+    ou_outdoor_space = models.ForeignKey(OutdoorSpace, on_delete=models.CASCADE)
+    out_value = models.CharField("Value", max_length=10, null=False)
