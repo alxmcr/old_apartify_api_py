dc46c6e175d2a2ead524a46367a957ee8a7487cf
Alejandro M. Coca <alxmcr@outlook.com>
2021-08-12 19:02:12 -0400

    + Views

--- api/views.py
+++ api/views.py
@@ -1,8 +1,44 @@
-from api.serializers import CountrySerializer
+from api.serializers import ApartmentSerializer, CitySerializer, CountrySerializer, FeatureSerializer, FlatSerializer, InvestmentSerializer, NeighborhoodSerializer, OutdoorSpaceSerializer, PhotoSerializer, StateSerializer
 from rest_framework import generics
-from api.models import Country
+from api.models import Apartment, City, Country, Feature, Flat, Investment, Neighborhood, OutdoorSpace, Photo, State
 
 
 class CountryList(generics.ListCreateAPIView):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
+
+class StateList(generics.ListCreateAPIView):
+    queryset = State.objects.all()
+    serializer_class = StateSerializer
+
+class CityList(generics.ListCreateAPIView):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+
+class NeighborhoodList(generics.ListCreateAPIView):
+    queryset = Neighborhood.objects.all()
+    serializer_class = NeighborhoodSerializer
+
+class FeatureList(generics.ListCreateAPIView):
+    queryset = Feature.objects.all()
+    serializer_class = FeatureSerializer
+
+class OutdoorSpaceList(generics.ListCreateAPIView):
+    queryset = OutdoorSpace.objects.all()
+    serializer_class = OutdoorSpaceSerializer
+
+class InvestmentList(generics.ListCreateAPIView):
+    queryset = Investment.objects.all()
+    serializer_class = InvestmentSerializer
+
+class ApartmentList(generics.ListCreateAPIView):
+    queryset = Apartment.objects.all()
+    serializer_class = ApartmentSerializer
+
+class PhotoList(generics.ListCreateAPIView):
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer
+
+class FlatList(generics.ListCreateAPIView):
+    queryset = Flat.objects.all()
+    serializer_class = FlatSerializer
