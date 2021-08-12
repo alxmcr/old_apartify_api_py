89ad417bff5a036074e0a0b457ba72d0e204aab4
Alejandro M. Coca <alxmcr@outlook.com>
2021-08-12 19:02:19 -0400

    + URLS

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("country", views.CountryList.as_view()),
+    path("countries", views.CountryList.as_view()),
+    path("states", views.StateList.as_view()),
+    path("cities", views.CityList.as_view()),
+    path("neighborhoods", views.NeighborhoodList.as_view()),
+    path("features", views.FeatureList.as_view()),
+    path("outdoor_spaces", views.OutdoorSpaceList.as_view()),
+    path("investments", views.InvestmentList.as_view()),
+    path("apartments", views.ApartmentList.as_view()),
+    path("photos", views.PhotoList.as_view()),
+    path("flats", views.FlatList.as_view()),
 ]
