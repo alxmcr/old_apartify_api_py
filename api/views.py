d91a0364365de3b6090c90878178a86646c3447a
Alejandro M. Coca <alxmcr@outlook.com>
2021-08-12 17:43:31 -0400

    + Views

--- api/views.py
+++ api/views.py
@@ -1,8 +1,12 @@
 from rest_framework import generics
-from api.serializers import PersonSerializer
-from api.models import Person
+from api.serializers import CountrySerializer, PersonSerializer
+from api.models import Country, Person
 
 
 class PersonList(generics.ListCreateAPIView):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
+
+class CountryList(generics.ListCreateAPIView):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
