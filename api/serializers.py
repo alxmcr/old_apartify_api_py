17b0b304d09d4215fb4738385871710513cf5b7a
Alejandro M. Coca <alxmcr@outlook.com>
2021-08-12 17:43:25 -0400

    + Serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,4 @@
-from api.models import Person
+from api.models import Country, Person, State
 from rest_framework import serializers
 
 
@@ -8,4 +8,26 @@ class PersonSerializer(serializers.ModelSerializer):
         fields = [
             'name',
             'lastname'
+        ]
+
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = [
+            "co_country",
+            "co_name",
+            "co_abbr",
+            "co_url",
+            "co_alt",
+        ]
+
+class StateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = State
+        fields = [
+            "st_state",
+            "st_name",
+            "st_abbr",
+            "st_url",
+            "st_alt",
         ]
