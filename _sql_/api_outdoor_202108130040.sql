feda1c38456b7e138495965d2c08998b3c036a56
Alejandro M. Coca <alxmcr@outlook.com>
2021-08-13 00:40:50 -0400

    Outdoor - many to many outdoor

---
+++ _sql_/api_outdoor_202108130040.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.api_outdoor (out_value,out_is_visible,out_is_card,ap_apartment_id,ou_outdoor_space_id) VALUES
+	 ('3',true,true,1,1),
+	 ('Sí',true,true,1,2),
+	 ('Sí',true,true,1,3),
+	 ('Sí',true,true,1,4),
+	 ('Sí',true,true,1,5),
+	 ('No',true,true,1,6);
