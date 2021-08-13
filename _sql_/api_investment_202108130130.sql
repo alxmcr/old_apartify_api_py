04434fe03577de35e6609873b256e55e1ca1be94
Alejandro M. Coca <alxmcr@outlook.com>
2021-08-13 01:31:15 -0400

    + api_investment

---
+++ _sql_/api_investment_202108130130.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.api_investment (in_type,in_name,in_icon_url,in_icon_color) VALUES
+	 ('Currency','Renta diaria corta estancia','https://res.cloudinary.com/images-alex-projects/image/upload/v1627708184/Portfolio/appartify-assets/svg/key_vtnoyk.svg','#f32321'),
+	 ('Number','Tasa de ocupación','https://res.cloudinary.com/images-alex-projects/image/upload/v1627708184/Portfolio/appartify-assets/svg/key_vtnoyk.svg','#00ff21'),
+	 ('Currency','Renta mensual larga estancia','https://res.cloudinary.com/images-alex-projects/image/upload/v1627708184/Portfolio/appartify-assets/svg/key_vtnoyk.svg','#0f00f1'),
+	 ('Currency','Mantenimiento mensual','https://res.cloudinary.com/images-alex-projects/image/upload/v1627708184/Portfolio/appartify-assets/svg/key_vtnoyk.svg','#0ffff1');
