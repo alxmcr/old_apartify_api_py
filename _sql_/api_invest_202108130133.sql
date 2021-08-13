a67c8383716c53f3cb771839b356289a93f4e2c4
Alejandro M. Coca <alxmcr@outlook.com>
2021-08-13 01:34:01 -0400

    + Invest

---
+++ _sql_/api_invest_202108130133.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.api_invest (inv_value,inv_is_visible,inv_is_card,ap_apartment_id,in_investment_id) VALUES
+	 ('$1,911',true,true,1,1),
+	 ('0.46',true,true,1,2),
+	 ('$27,423',true,true,1,3),
+	 ('$2,505.00',true,true,1,4);
