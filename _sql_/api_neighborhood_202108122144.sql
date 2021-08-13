46af187f82c13b0cafac6a454f8506b5640e9db1
Alejandro M. Coca <alxmcr@outlook.com>
2021-08-12 21:51:08 -0400

    + SQL feature, state, and neighborhood

---
+++ _sql_/api_neighborhood_202108122144.sql
@@ -0,0 +1,13 @@
+INSERT INTO public.api_neighborhood (ne_name,ne_abbr,ne_url,ne_alt) VALUES
+	 ('Napoles','NAP','',''),
+	 ('Hipódromo Condesa','HCON','',''),
+	 ('Lomas del Chamizal','LCH','',''),
+	 ('San Juan','SJ','',''),
+	 ('Anáhuac','AN','',''),
+	 ('Jesús del Monte','JM','',''),
+	 ('8 de Agosto','8A','',''),
+	 ('Anzures','ANZ','',''),
+	 ('Del Valle Sur','DVS','',''),
+	 ('Narvarte Poniente','NP','','');
+INSERT INTO public.api_neighborhood (ne_name,ne_abbr,ne_url,ne_alt) VALUES
+	 ('Paseo de las Lomas','PALO','','');
---
+++ _sql_/api_state_202108122143.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.api_state (st_name,st_abbr,st_url,st_alt) VALUES
+	 ('Estado de México','ECDMX','','');
