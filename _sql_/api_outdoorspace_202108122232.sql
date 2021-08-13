4af73b22a8f2ff7fc7f46c30e4f9f066f29abc93
Alejandro M. Coca <alxmcr@outlook.com>
2021-08-12 22:33:24 -0400

    + api_outdoorspace

---
+++ _sql_/api_outdoorspace_202108122232.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.api_outdoorspace (ou_type,ou_name,ou_icon_url,ou_icon_color) VALUES
+	 ('Number','Nivel','',''),
+	 ('string','Balcón','',''),
+	 ('string','Elevador','',''),
+	 ('string','Seguridad','',''),
+	 ('string','Rooftop','',''),
+	 ('string','Terraza privada','','');
