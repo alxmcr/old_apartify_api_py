9547ea6ad65aec9531d154f3dfc6f5d09ae30279
Alejandro M. Coca <alxmcr@outlook.com>
2021-08-12 23:53:32 -0400

    City Hall

---
+++ _sql_/api_cityhall_202108122353.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.api_cityhall (ch_name,ch_abbr,ch_url,ch_alt) VALUES
+	 ('Cuauhtémoc','CUA','',''),
+	 ('Miguel Hidalgo','MIHI','',''),
+	 ('Cuajimalpa','CUAJ','',''),
+	 ('Benito Juárez','BEJU','','');
