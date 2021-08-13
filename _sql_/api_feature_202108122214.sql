8de6a44072c572bfb68d3ed0a2992b7472d6ddd6
Alejandro M. Coca <alxmcr@outlook.com>
2021-08-12 22:14:47 -0400

    Feature

---
+++ _sql_/api_feature_202108122214.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.api_feature (fe_type,fe_name,fe_icon_url,fe_icon_color) VALUES
+	 ('Number','Tamaño','https://res.cloudinary.com/images-alex-projects/image/upload/v1627705301/Portfolio/appartify-assets/svg/room-plan_vyen5l.svg','#FF9671'),
+	 ('Number','Habitaciones','https://res.cloudinary.com/images-alex-projects/image/upload/v1627705301/Portfolio/appartify-assets/svg/room-furniture_zz4hiz.svg','#D65DB1'),
+	 ('Number','Lugares de estacionamientos','https://res.cloudinary.com/images-alex-projects/image/upload/v1627705301/Portfolio/appartify-assets/svg/car_afbqde.svg','#0089BA'),
+	 ('Number','Bathrooms','https://res.cloudinary.com/images-alex-projects/image/upload/v1627705301/Portfolio/appartify-assets/svg/bathtub_kmoorx.svg','#C34A36'),
+	 ('String','Tipo de Propiedad','https://res.cloudinary.com/images-alex-projects/image/upload/v1627705301/Portfolio/appartify-assets/svg/bathtub_kmoorx.svg','#00C9A7'),
+	 ('Number','Año de construcción','','#257A3E');
