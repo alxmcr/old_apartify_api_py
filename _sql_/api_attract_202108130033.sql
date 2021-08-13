272b66b5749a40e53f04038b5032599720b77d05
Alejandro M. Coca <alxmcr@outlook.com>
2021-08-13 00:34:16 -0400

    api_attract

---
+++ _sql_/api_attract_202108130033.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.api_attract (att_value,att_is_visible,att_is_card,ap_apartment_id,fe_feature_id) VALUES
+	 ('93 m²',true,true,1,1),
+	 ('2',true,true,1,2),
+	 ('1',true,true,1,3),
+	 ('2',true,true,1,4),
+	 ('Departamento',true,true,1,5),
+	 ('2018',true,true,1,6);
