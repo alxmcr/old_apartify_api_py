557f2bf6da36c13bf80d1563c0aec6a5d96e33df
Alejandro M. Coca <alxmcr@outlook.com>
2021-08-13 18:21:19 -0400

    + SQL Photos

---
+++ _sql_/api_photo_202108131821.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.api_photo (ph_url,ph_alt,ap_apartment_id) VALUES
+	 ('https://res.cloudinary.com/images-alex-projects/image/upload/v1627708883/Portfolio/appartify-assets/images/apart-0001-img-002_lyks9l.jpg','Room-001',1),
+	 ('https://res.cloudinary.com/images-alex-projects/image/upload/v1627708883/Portfolio/appartify-assets/images/apart-0001-img-001_fgyity.jpg','Room-002',1);
