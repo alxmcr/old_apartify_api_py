2b7f83b9f433a5f999adf583a97a4814693b871d
Alejandro M. Coca <alxmcr@outlook.com>
2021-08-20 17:15:05 -0400

    Add filters to admin views

--- api/views.py
+++ api/views.py
@@ -14,7 +14,7 @@ class CountryList(generics.ListCreateAPIView):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['co_name']
+    filterset_fields = ['co_name', 'co_abbr']
 
 class CountryDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Country.objects.all()
@@ -23,6 +23,8 @@ class CountryDetails(generics.RetrieveUpdateDestroyAPIView):
 class StateList(generics.ListCreateAPIView):
     queryset = State.objects.all()
     serializer_class = StateSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['st_name', 'st_abbr']
 
 class StateDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = State.objects.all()
@@ -31,6 +33,8 @@ class StateDetails(generics.RetrieveUpdateDestroyAPIView):
 class CityList(generics.ListCreateAPIView):
     queryset = City.objects.all()
     serializer_class = CitySerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['ci_name', 'ci_abbr']
 
 class CityDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = City.objects.all()
@@ -39,6 +43,8 @@ class CityDetails(generics.RetrieveUpdateDestroyAPIView):
 class CityHallList(generics.ListCreateAPIView):
     queryset = CityHall.objects.all()
     serializer_class = CityHallSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['ch_name', 'ch_abbr']
 
 class CityHallDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = CityHall.objects.all()
@@ -47,6 +53,8 @@ class CityHallDetails(generics.RetrieveUpdateDestroyAPIView):
 class NeighborhoodList(generics.ListCreateAPIView):
     queryset = Neighborhood.objects.all()
     serializer_class = NeighborhoodSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['ne_name', 'ne_abbr']
 
 class NeighborhoodDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Neighborhood.objects.all()
@@ -55,6 +63,8 @@ class NeighborhoodDetails(generics.RetrieveUpdateDestroyAPIView):
 class FeatureList(generics.ListCreateAPIView):
     queryset = Feature.objects.all()
     serializer_class = FeatureSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['fe_type', 'fe_name']
 
 class FeatureDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Feature.objects.all()
@@ -63,6 +73,8 @@ class FeatureDetails(generics.RetrieveUpdateDestroyAPIView):
 class OutdoorSpaceList(generics.ListCreateAPIView):
     queryset = OutdoorSpace.objects.all()
     serializer_class = OutdoorSpaceSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['ou_type', 'ou_name']
 
 class OutdoorSpaceDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = OutdoorSpace.objects.all()
@@ -71,6 +83,8 @@ class OutdoorSpaceDetails(generics.RetrieveUpdateDestroyAPIView):
 class InvestmentList(generics.ListCreateAPIView):
     queryset = Investment.objects.all()
     serializer_class = InvestmentSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['in_type', 'in_name']
 
 class InvestmentDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Investment.objects.all()
@@ -79,6 +93,8 @@ class InvestmentDetails(generics.RetrieveUpdateDestroyAPIView):
 class ApartmentList(generics.ListCreateAPIView):
     queryset = Apartment.objects.all()
     serializer_class = ApartmentSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['ap_cost_offer', 'ap_cost_list', 'ap_floor_number']
 
 class ApartmentDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Apartment.objects.all()
@@ -87,6 +103,8 @@ class ApartmentDetails(generics.RetrieveUpdateDestroyAPIView):
 class PhotoList(generics.ListCreateAPIView):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['ap_apartment', 'ph_url']
 
 class PhotoDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Photo.objects.all()
@@ -95,6 +113,8 @@ class PhotoDetails(generics.RetrieveUpdateDestroyAPIView):
 class FloorPlanList(generics.ListCreateAPIView):
     queryset = FloorPlan.objects.all()
     serializer_class = FloorPlanSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['ap_apartment', 'fp_url']
 
 class FloorPlanDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = FloorPlan.objects.all()
@@ -104,6 +124,8 @@ class FloorPlanDetails(generics.RetrieveUpdateDestroyAPIView):
 class AttractList(generics.ListCreateAPIView):
     queryset = Attract.objects.all()
     serializer_class = AttractSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['ap_apartment', 'fe_feature']
 
 class AttractDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Attract.objects.all()
@@ -112,6 +134,8 @@ class AttractDetails(generics.RetrieveUpdateDestroyAPIView):
 class OutdoorList(generics.ListCreateAPIView):
     queryset = Outdoor.objects.all()
     serializer_class = OutdoorSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['ap_apartment', 'ou_outdoor_space']
 
 class OutdoorDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Outdoor.objects.all()
@@ -120,6 +144,8 @@ class OutdoorDetails(generics.RetrieveUpdateDestroyAPIView):
 class InvestList(generics.ListCreateAPIView):
     queryset = Invest.objects.all()
     serializer_class = InvestSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['ap_apartment', 'in_investment']
 
 class InvestDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Invest.objects.all()
