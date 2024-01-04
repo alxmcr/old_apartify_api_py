f07bb250449bf4cb6fa5a61f17debf17935f6524
Alejandro M. Coca <alxmcr@outlook.com>
2024-01-04 17:34:53 -0400

    update cors

--- apartify_api_py/settings/dev.py
+++ apartify_api_py/settings/dev.py
@@ -138,6 +138,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://localhost:3000",
+    "http://localhost:5173",
     "http://127.0.0.1:9000",
     "https://apartify-app.netlify.app",
 ]
